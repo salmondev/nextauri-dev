853764aae6e0f3a5d234d6d7cee66e9cac793e49
panurut-cnk <panurut.ch@gmail.com>
2025-03-13 10:52:19 +0700

    save

--- src-next/containers/Home/index.tsx
+++ src-next/containers/Home/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { generatePassword } from 'utils/password';
 import { Copy, Check } from 'lucide-react';
 import './style.css';
